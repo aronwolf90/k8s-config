0ef032646e90520bc4cfe084861851bdfe908273
Aron Wolf <aronwolf90@gmail.com>
2021-01-23 23:38:21 +0100

    feature: add multi master support

--- hcloud.tf
+++ hcloud.tf
@@ -30,16 +30,43 @@ resource "hcloud_server" "master" {
   ssh_keys    = [hcloud_ssh_key.default.id]
 }
 
-resource "hcloud_floating_ip" "master" {
-  name      = "masterv1"
-  type      = "ipv4"
-  server_id = hcloud_server.master.id
+resource "hcloud_server" "master2" {
+  name        = "master2"
+  image       = "ubuntu-20.04"
+  server_type = "cx21"
+  ssh_keys    = [hcloud_ssh_key.default.id]
+}
+
+resource "hcloud_load_balancer" "master" {
+  name               = "master"
+  load_balancer_type = "lb11"
+  depends_on         = [hcloud_server.master, hcloud_server.master2]
+  location           = "nbg1"
+}
+
+resource "hcloud_load_balancer_target" "master" {
+  type             = "server"
+  load_balancer_id = hcloud_load_balancer.master.id
+  server_id        = hcloud_server.master.id
+}
+
+resource "hcloud_load_balancer_target" "master2" {
+  type             = "server"
+  load_balancer_id = hcloud_load_balancer.master.id
+  server_id        = hcloud_server.master2.id
+}
+
+resource "hcloud_load_balancer_service" "load_balancer_service" {
+  load_balancer_id = hcloud_load_balancer.master.id
+  protocol         = "tcp"
+  listen_port      = 6443
+  destination_port = 6443
 }
 
 resource "null_resource" "install_kubernetes" {
   depends_on = [
     hcloud_server.master,
-    hcloud_floating_ip.master
+    hcloud_load_balancer.master
   ]
 
   triggers = {
@@ -48,6 +75,46 @@ resource "null_resource" "install_kubernetes" {
 
   connection {
     host        = hcloud_server.master.ipv4_address
+    type        = "ssh"
+    user        = "root"
+    private_key = file(var.private_key)
+  }
+  provisioner "file" {
+    source      = "install_kubedm.rb"
+    destination = "/tmp/install_kubedm.rb"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      # Install
+      "bash /tmp/install_kubedm.rb",
+      "kubeadm init --control-plane-endpoint ${hcloud_load_balancer.master.ipv4}:6443 --upload-certs --ignore-preflight-errors=DirAvailable--var-lib-etcd --pod-network-cidr=10.244.0.0/16",
+      "curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\"",
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.20.0/bin/linux/amd64/kubectl",
+      "chmod +x ./kubectl",
+      "sudo mv ./kubectl /usr/local/bin/kubectl",
+      "mkdir -p $HOME/.kube",
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+      "kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml",
+      "kubectl create secret generic hcloud-csi -n kube-system --from-literal=token=${var.hcloud_token}",
+      "kubectl apply -f https://raw.githubusercontent.com/hetznercloud/csi-driver/v1.5.1/deploy/kubernetes/hcloud-csi.yml"
+    ]
+  }
+}
+
+resource "null_resource" "install_master2" {
+  depends_on = [
+    hcloud_server.master2,
+    null_resource.install_kubernetes,
+    hcloud_load_balancer.master
+  ]
+
+  triggers = {
+    hcloud_server_master_id = hcloud_server.master2.id
+  }
+
+  connection {
+    host        = hcloud_server.master2.ipv4_address
     type        = "ssh"
     user        = "root"
     private_key = file(var.private_key)
@@ -66,32 +133,19 @@ resource "null_resource" "install_kubernetes" {
   }
   provisioner "remote-exec" {
     inline = [
-      "ip addr add ${hcloud_floating_ip.master.ip_address} dev eth0",
-      "mkdir -p /etc/kubernetes/pki/",
-      "cp /tmp/ca.crt /etc/kubernetes/pki/ca.crt",
-      "cp /tmp/ca.key /etc/kubernetes/pki/ca.key",
-      # Install
       "bash /tmp/install_kubedm.rb",
-      "kubeadm init --apiserver-advertise-address ${hcloud_floating_ip.master.ip_address} --ignore-preflight-errors=DirAvailable--var-lib-etcd --pod-network-cidr=10.244.0.0/16",
-      "curl -LO \"https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl\"",
-      "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.20.0/bin/linux/amd64/kubectl",
-      "chmod +x ./kubectl",
-      "sudo mv ./kubectl /usr/local/bin/kubectl",
-      "mkdir -p $HOME/.kube",
-      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
-      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
-      "kubectl annotate node master flannel.alpha.coreos.com/public-ip-overwrite=${hcloud_floating_ip.master.ip_address}",
-      "kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml",
-      "kubectl create secret generic hcloud-csi -n kube-system --from-literal=token=${var.hcloud_token}",
-      "kubectl apply -f https://raw.githubusercontent.com/hetznercloud/csi-driver/v1.5.1/deploy/kubernetes/hcloud-csi.yml"
+      "echo ${data.external.kubeadm_join_master.result.command}",
+      data.external.kubeadm_join_master.result.command
     ]
   }
 }
 
-resource "hcloud_server" "node1" {
+resource "hcloud_server" "node" {
   depends_on   = [null_resource.install_kubernetes]
 
-  name        = "node1"
+  count = 3
+
+  name        = "node${count.index}"
   image       = "ubuntu-20.04"
   server_type = "cx31"
   ssh_keys    = [hcloud_ssh_key.default.id]
@@ -112,6 +166,17 @@ resource "hcloud_server" "node1" {
       data.external.kubeadm_join.result.command
     ]
   }
+}
+
+data "external" "kubeadm_join_master" {
+  program = ["./kubeadm_master_token.sh"]
+
+  query = {
+    host = hcloud_server.master.ipv4_address
+    key = var.private_key
+  }
+
+  depends_on = [null_resource.install_kubernetes]
 }
 
 data "external" "kubeadm_join" {
