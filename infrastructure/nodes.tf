5e24e86986fdef7087267854a36544abe541c95e
Aron Wolf <aronwolf90@gmail.com>
2023-10-04 21:06:56 +0000

    refactore: Move ssh keys to different file

--- infrastructure/nodes.tf
+++ infrastructure/nodes.tf
@@ -1,10 +1,3 @@
-resource "hcloud_ssh_key" "ssh_public_keys" {
-  for_each = {for public_ssh_key in local.public_ssh_keys : public_ssh_key.name => public_ssh_key}
-
-  name       = each.value.name
-  public_key = each.value.key
-}
-
 resource "hcloud_server" "nodes" {
   for_each = var.nodes
 
